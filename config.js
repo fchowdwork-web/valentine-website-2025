36dc81469d6d282d1d8ce5f219b4c2ad59178f13
fchowdwork-web <fchowdwork@gmail.com>
2026-02-01 11:03:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Come Bounce on it WOOO HOOO",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
